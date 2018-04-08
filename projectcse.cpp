a061bedab5e36a4395d448a6f605ce47ccca181e
RishabhSingh98 <38190537+RishabhSingh98@users.noreply.github.com>
2018-04-08 13:38:53 -0700

    first file added

---
+++ projectcse.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<string.h>
+class Service_type
+{
+	int service_query;
+	char vechicle;
+	public:
+		void display()
+		{
+			cout<<"\n1.Accidental Vehicle\n2.Regular Service\n3.Normal Checkup.\n4.Denting and Painting\n5.Washing\n6.Part Replacement.\n";
+            cin>>service_query;
+		}
+		class derived:public Service_type
+		{
+			public:
+			void Accindental()
+			{
+				int AC,UI;
+			const String type="Accidential Case";
+			cout<<"Vehicle Name";
+			get_line(cin,vehicle name )
+			cout<<"Enter The Amount of Accidential Case";
+			cin>>AC;
+			cout<<"User Unique Id";
+			cin>>"UI";
+		}
+		void regular()
+        {
+			int RS,UI
+			const String type="Regular Service";
+			cout<<"Vehicle Name";
+			get_line(cin,vehicle name )
+			cout<<"Enter The Amount of Regular Service";
+			cin>>RS;
+			cout<<"User Unique Id";
+			cin>>"UI";
+		}
+		void norm
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		
+		
+		}
+}
